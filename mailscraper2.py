4210d46fe7e8db91947b80c3976205a7a6290100
Carrefax <daniel.hoadley@iclr.co.uk>
2018-02-16 21:27:06 +0000

    Repo created

---
+++ mailscraper2.py
@@ -0,0 +1,50 @@
+import feedparser
+from bs4 import BeautifulSoup
+from lxml import html
+import requests
+import re
+
+import csv
+
+words = ['camel trench coat', 'Public Access Line',]
+links_list = []
+hits = []
+hit_link = []
+
+d = feedparser.parse('http://www.dailymail.co.uk/articles.rss')
+
+for item in d.entries:
+    link = ( item[ "link" ] )
+    links_list.append(link)
+
+for link in links_list:
+    page = requests.get(link)
+    tree = html.fromstring(page.content)
+
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    text = soup.find('body')
+    text = text.text
+
+    for word in words:
+        regex = r"\b"+ re.escape(word) + r"\b"
+        match = re.search(regex, text)
+        if match:
+            print ("________found")
+            hit_article.append(title)
+            hits.append(word)
+            hit_link.append(str(link))
+
+match_dictionary = dict(zip(hit_link, hits))
+
+print (match_dictionary)
+
+w = csv.writer(open("output.csv", "w"))
+for key, val in match_dictionary.items():
+    w.writerow([key, val])
+
+
+
+
+
+
